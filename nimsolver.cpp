31836ceac589a896fb495722f75fc7015d0a8606
jsekerak <jjsekerak@gmail.com>
2013-08-23 15:17:21 +0200

    Complete game able to be played. No help functions or checks.

--- nimsolver.cpp
+++ nimsolver.cpp
@@ -10,27 +10,49 @@ This is a practice c++ script to see how much i've learned
 
 using namespace std;
 
+int takeaway (int a, int b) { // a = # of flags, b = # to remove
+	return (a-b);
+}
 
 int main()
 {
-	int numPiles, n, totalflags;
-	int * p;
+	int numPiles=0, n=0, totalFlags=0, pileSelect=0, takeFlags=0;
+	int * pilesArray;
 	cout << "How many piles will you play with?: ";
 	cin >> numPiles;
 	pilesArray = new (nothrow) int[numPiles];
+
 	if (pilesArray == 0)
 		cout << "Error: memory could not be allocated.";
 	else {
 		for (n=0; n<numPiles; n++) {
 			cout << "How many flags in Pile " << n << ": ";
 			cin >> pilesArray[n];
+			totalFlags = totalFlags + pilesArray[n];
 		}
-		cout << "You have entered: \n";
+		cout << "You have entered " << totalFlags << " total flags: \n";
 		for (n=0; n<numPiles; n++) {
 			cout << pilesArray[n] << " in Pile " << n << "\n";
 		}
-		delete[] p;
 	}
+
+	do {
+		cout << "How many flags will you take?: ";
+		cin >> takeFlags;
+		cout << "From which pile?: ";
+		cin >> pileSelect;
+
+		pilesArray[pileSelect] = pilesArray[pileSelect] - takeFlags;
+		totalFlags = totalFlags - takeFlags;
+
+		cout << "There are " << totalFlags << " flags left.\n";
+		for (n=0; n<numPiles; n++) {
+			cout << pilesArray[n] << " in Pile " << n << "\n";
+		}
+
+	} while (totalFlags > 0);
+
+
 
 	return 0;
 }
