c6f8da24d3f163f7067bdeef7825bad8cde2260d
jsekerak <jjsekerak@gmail.com>
2013-08-23 14:38:04 +0200

    Choose # of piles and # flags in each pile

--- nimsolver.cpp
+++ nimsolver.cpp
@@ -4,7 +4,33 @@ This is a practice c++ script to see how much i've learned
 */
 
 #include <iostream>
-#include <strings>
+#include <string>
 #include <sstream>
+#include <new>
 
-namespace std;
+using namespace std;
+
+
+int main()
+{
+	int numPiles, n, totalflags;
+	int * p;
+	cout << "How many piles will you play with?: ";
+	cin >> numPiles;
+	pilesArray = new (nothrow) int[numPiles];
+	if (pilesArray == 0)
+		cout << "Error: memory could not be allocated.";
+	else {
+		for (n=0; n<numPiles; n++) {
+			cout << "How many flags in Pile " << n << ": ";
+			cin >> pilesArray[n];
+		}
+		cout << "You have entered: \n";
+		for (n=0; n<numPiles; n++) {
+			cout << pilesArray[n] << " in Pile " << n << "\n";
+		}
+		delete[] p;
+	}
+
+	return 0;
+}
